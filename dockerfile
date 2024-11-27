3f75eebbd52b3c148ef5ff6af0e1fb5e648618a5
shivi <shiviell@gmail.com>
2024-11-27 20:00:00 +0200

    modbus-exporter set

--- dockerfile
+++ dockerfile
@@ -1,7 +1,4 @@
-FROM python:3
-
-# Install supervisor
-RUN apt-get update && apt-get install -y supervisor
+FROM python:3.8-slim
 
 # Set the working directory inside the container
 WORKDIR /app
@@ -13,8 +10,5 @@ COPY . /app/
 # Install any necessary dependencies (if needed)
 RUN pip install -r requirements.txt
 
-# Configure supervisord
-COPY supervisord.conf /etc/supervisor/conf.d/supervisord.conf
-
-# Run supervisord
+# Run server
 CMD ["supervisord", "-c", "/etc/supervisor/conf.d/supervisord.conf"]
--- server.py
+++ server.py
@@ -1,20 +1,54 @@
 # Python 3 server example
 from http.server import BaseHTTPRequestHandler, HTTPServer
+#import yaml
+import json
 #import time
+import subprocess
+import glob
+import urllib.parse
 
 hostName = "0.0.0.0"
-serverPort = 9080
+serverPort = 9502
+yml_line = {}
+prom_data = ""
+
+def prom_exporter (module,target,slaveID,byteOrder):
+    metric = ""
+    files = glob.glob('./reg/%s*.modbus' % (module))
+    for file in files:
+        # Extract the command from the payload
+        command = "modbus -r %s -s %i -B %s %s \* -t 1" % (file,slaveID,byteOrder,target,)
+        if not command:
+            raise ValueError("Missing 'command' in payload")
+        # Execute the command
+        result = subprocess.run(command, shell=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        a = result.stdout.split('\n')
+        for line in a:
+            b = line.strip().split(': ')
+            c = b[0].split('_')
+            if isinstance(c[-1], int):
+                metric =+ '%s{IO="%s",address="%s",cell="%s"} %s\n' % ('_'.join(c[2:-1]),c[1],c[0],c[-1],b[1])
+            else:
+                metric =+ '%s{IO="%s",address="%s""} %s\n' % ('_'.join(c[2:]),c[1],c[0],b[1])
+    return metric 
+
+                    
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
         print (self.path)
         if self.path == '/metrics':
+                    # Parse the URL to extract the query string
+            url_parts = urllib.parse.urlparse(self.path)
+            query_params = urllib.parse.parse_qs(url_parts.query)
+            metrics = prom_exporter (query_params['module'],query_params['target'],query_params['sub_target'],query_params['byteOrder'])
             self.send_response(200)
             self.send_header("Content-type", "text/plain; version=0.0.4; charset=utf-8")
             self.end_headers()
             #self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-            with open('metrics.txt', 'rb') as file: 
-                self.wfile.write(file.read()) # Read the file and send the contents 
+            #with open('metrics.txt', 'rb') as file: 
+            #    self.wfile.write(file.read()) # Read the file and send the contents 
+            self.wfile.write(metrics) # Read the file and send the contents 
         elif self.path == '/favicon.ico':
             self.send_response(200)
             self.send_header("Content-type", "image/x-icon")
@@ -29,7 +63,50 @@ class MyServer(BaseHTTPRequestHandler):
             with open('defult.html', 'rb') as file: 
                 self.wfile.write(file.read()) # Read the file and send the contents 
 
-if __name__ == "__main__":        
+    def do_POST(self):
+        try:
+            # Get the content length
+            content_length = int(self.headers.get('Content-Length', 0))
+            
+            # Read the payload
+            post_data = self.rfile.read(content_length) if content_length > 0 else b'{}'
+            
+            # Parse JSON payload
+            params = json.loads(post_data.decode('utf-8'))
+            
+            # Extract the command from the payload
+            command = "modbus -s %i -B %s %s %s -t 1" % (params['slaveID'],params['byteOrder'], params['target'], ' '.join(params['access']))
+            if not command:
+                raise ValueError("Missing 'command' in payload")
+            
+            # Execute the command
+            result = subprocess.run(command, shell=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            
+            # Prepare the response
+            response = {
+                "stdout": result.stdout,
+                "stderr": result.stderr,
+                "returncode": result.returncode
+            }
+            
+            # Send the response
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps(response).encode('utf-8'))
+        
+        except Exception as e:
+            # Handle errors and send an error response
+            self.send_response(400)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+            error_response = {"error": str(e)}
+            self.wfile.write(json.dumps(error_response).encode('utf-8'))
+
+
+if __name__ == "__main__":
+ 
+    
     webServer = HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
 
